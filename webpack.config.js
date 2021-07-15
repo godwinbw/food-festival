bbe5d5ad8e88877bf0a1e3dd130082061fa33451
Bryan Godwin <godwin.bryan.w@gmail.com>
2021-07-15 09:45:41 -0500

    updated to 19.2

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,17 @@
+const webpack = require("webpack")
 const path = require("path");
-const webpack = require("webpack");
 
 module.exports = {
   entry: "./assets/js/script.js",
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "main.bundle.js",
-    sourceMapFilename: "[name].js.map",
+    path: path.join(__dirname + "/dist"),
+    filename: "main.bundle.js"
   },
   plugins: [
-    new webpack.ProvidePlugin({
-      $: "jquery",
-      jQuery: "jquery",
-    }),
+  new webpack.ProvidePlugin({
+    $: "jquery",
+    jQuery: "jquery"
+  }),
   ],
-  mode: "development",
-  devtool: "source-map",
+  mode: "development"
 };
